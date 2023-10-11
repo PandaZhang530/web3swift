14f3a8037e7e809eb450ef7b7087f45ae5af5ed3
JackPanda8 <zp838315760@163.com>
2023-10-11 17:05:18 +0800

    [feat]: change dependency of secp256k1 from local source files to spm url

--- Package.swift
+++ Package.swift
@@ -13,43 +13,29 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMinor(from: "5.3.0")),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.5.1"))
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.5.1")),
+        .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.1"),
     ],
     targets: [
-        .target(name: "secp256k1_alias"),
         .target(
             name: "Web3Core",
-            dependencies: ["BigInt", "secp256k1_alias", "CryptoSwift"]
+            dependencies: [
+                .product(name: "BigInt", package: "BigInt"),
+                .product(name: "secp256k1", package: "secp256k1"),
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
+            ]
         ),
         .target(
             name: "web3swift",
-            dependencies: ["Web3Core", "BigInt", "secp256k1_alias"],
+            dependencies: [
+                .target(name: "Web3Core"),
+                .product(name: "BigInt", package: "BigInt"),
+                .product(name: "secp256k1", package: "secp256k1"),
+            ],
             resources: [
                 .copy("./Browser/browser.js"),
                 .copy("./Browser/browser.min.js"),
                 .copy("./Browser/wk.bridge.min.js")
-            ]
-        ),
-        .testTarget(
-            name: "localTests",
-            dependencies: ["web3swift"],
-            path: "Tests/web3swiftTests/localTests",
-            resources: [
-                .copy("../../../TestToken/Helpers/SafeMath/SafeMath.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/ERC20.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/IERC20.sol"),
-                .copy("../../../TestToken/Token/Web3SwiftToken.sol")
-            ]
-        ),
-        .testTarget(
-            name: "remoteTests",
-            dependencies: ["web3swift"],
-            path: "Tests/web3swiftTests/remoteTests",
-            resources: [
-                .copy("../../../TestToken/Helpers/SafeMath/SafeMath.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/ERC20.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/IERC20.sol"),
-                .copy("../../../TestToken/Token/Web3SwiftToken.sol")
             ]
         )
     ]
